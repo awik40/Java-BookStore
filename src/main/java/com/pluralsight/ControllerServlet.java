8450e9d3fc39703f1de7ef860e3ef5eb279ffae5
Wiklund, Andreas <Andreas.Wiklund@teliacompany.com>
2019-08-23 13:07:13 +0200

    Completed the module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -53,12 +53,16 @@ public class ControllerServlet extends HttpServlet {
 				case "/admin":
 					 showBookAdmin(request, response);
            break;
-			  case "/new":
-					showNewForm(request, response);
-          break;
+			    case "/new":
+					 showNewForm(request, response);
+           break;
 				case "/insert":
-					insertBook(request, response);
-          break;
+					 insertBook(request, response);
+           break;
+
+                case "/delete":
+					 deleteBook(request, response);
+           break;
         default:
 				   listBooks(request, response);
            break;
@@ -102,6 +106,15 @@ public class ControllerServlet extends HttpServlet {
 		Book newBook = new Book(title, author, Float.parseFloat(priceString));
 
 		bookDAO.insertBook(newBook);
+		response.sendRedirect("list");
+	}
+
+	private void deleteBook(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException{
+
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		bookDAO.deleteBook(id);
 		response.sendRedirect("list");
 	}
 
